8e2aaf371e9c02821788d2e334ed00c5e11d5239
Faiza Ambreen <faizaambreen.dev@gmail.com>
2024-11-28 09:35:56 -0800

    add alexendria

--- resources/views/master/main.blade.php
+++ resources/views/master/main.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ asset('images/favicons.png') }}" rel="icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alexandria:wght@100..900&display=swap" rel="stylesheet">
     <title>Mattbuysyourcar</title>
     <!-- Swiper CSS -->
     <link href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" rel="stylesheet">
